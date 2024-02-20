5c339ec5cb09e3c347898215fbe5e83a1fc7faf9
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 07:51:26 +0200

    dynamic name in the navbar

--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -1,11 +1,15 @@
 import Avatar from "../ui/Avatar";
 import SearchInput from "../ui/SearchInput";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 const Navbar = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
+
   return (
     <nav className="bg-gray-100 py-4 px-10 flex flex-row justify-between">
       <span className="text-3xl text-gray-500 font-semibold">
-        Welcome Talia,
+        Welcome {user},
       </span>
       <div className="flex flex-row items-center gap-10">
         <SearchInput />
