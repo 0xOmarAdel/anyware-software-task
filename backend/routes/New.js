ee41e6c661df08d8a828650415529db54650bbbc
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 12:34:05 +0200

    fixed a type

--- backend/routes/New.js
+++ backend/routes/New.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Quiz = require("../models/Quiz");
-const Assignment = require("../models/Announcement");
+const Announcement = require("../models/Announcement");
 
 router.get("/", async (req, res) => {
   const firstQuiz = await Quiz.findOne().sort("createdAt").limit(1);
 
-  const firstAssignment = await Assignment.findOne().sort("createdAt").limit(1);
+  const firstAssignment = await Announcement.findOne()
+    .sort("createdAt")
+    .limit(1);
 
   res.status(200).json({
     firstQuiz,
