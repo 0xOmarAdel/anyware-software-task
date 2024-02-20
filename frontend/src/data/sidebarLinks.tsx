5d04d86fe474518ba8b1ec912ef1b37c5ee3e643
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 06:36:57 +0200

    created sidebar links data file

---
+++ frontend/src/data/sidebarLinks.tsx
@@ -0,0 +1,49 @@
+import { IoHome } from "react-icons/io5";
+import {
+  FaCalendarDays,
+  FaBook,
+  FaGraduationCap,
+  FaBullhorn,
+} from "react-icons/fa6";
+import { BsGraphUpArrow } from "react-icons/bs";
+
+const sidebarLinks = [
+  {
+    id: 1,
+    text: "Dashboard",
+    path: "/dashboard",
+    icon: <IoHome className="text-xl" />,
+  },
+  {
+    id: 2,
+    text: "Schedule",
+    path: "/schedule",
+    icon: <FaCalendarDays className="text-xl" />,
+  },
+  {
+    id: 3,
+    text: "Courses",
+    path: "/courses",
+    icon: <FaBook className="text-xl" />,
+  },
+  {
+    id: 4,
+    text: "Gradebook",
+    path: "/gradebook",
+    icon: <FaGraduationCap className="text-xl" />,
+  },
+  {
+    id: 5,
+    text: "Performance",
+    path: "/performance",
+    icon: <BsGraphUpArrow className="text-xl" />,
+  },
+  {
+    id: 6,
+    text: "Announcements",
+    path: "/announcements",
+    icon: <FaBullhorn className="text-xl" />,
+  },
+];
+
+export default sidebarLinks;
