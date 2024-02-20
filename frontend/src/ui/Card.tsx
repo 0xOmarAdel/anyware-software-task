0828afd88942f3f11e217b981e437f8eed5d21ae
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 07:27:03 +0200

    added optional className prop to the card component

--- frontend/src/ui/Card.tsx
+++ frontend/src/ui/Card.tsx
@@ -1,10 +1,15 @@
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
 const Card: React.FC<Props> = (props) => {
   return (
-    <div className="h-fit p-5 bg-white shadow-lg rounded-xl text-center dark:bg-primarylessDark dark:text-textLighter">
+    <div
+      className={`h-fit p-5 bg-white shadow-lg rounded-xl text-center ${
+        props.className || ""
+      }`}
+    >
       {props.children}
     </div>
   );
