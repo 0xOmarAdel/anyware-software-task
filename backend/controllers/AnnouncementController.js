887cac268951861ca0b56545665729de49c921f8
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 10:31:09 +0200

    created get and delete routes

--- backend/controllers/AnnouncementController.js
+++ backend/controllers/AnnouncementController.js
@@ -2,15 +2,41 @@ const { StatusCodes } = require("http-status-codes");
 const { NotFoundError, BadRequestError } = require("../errors");
 const Announcement = require("../models/Announcement");
 
-const getAllAnnouncements = async (req, res) => {};
+const getAllAnnouncements = async (req, res) => {
+  const announcements = await Announcement.find().sort("createdAt");
 
-const getAnnouncement = async (req, res) => {};
+  res
+    .status(StatusCodes.OK)
+    .json({ announcements, count: announcements.length });
+};
+
+const getAnnouncement = async (req, res) => {
+  const announcementId = req.params.id;
+
+  const announcement = await Announcement.findOne({ _id: announcementId });
+
+  if (!announcement)
+    throw new NotFoundError(`No announcement with id ${announcementId}`);
+
+  res.status(StatusCodes.OK).json({ announcement });
+};
 
 const createAnnouncement = async (req, res) => {};
 
 const updateAnnouncement = async (req, res) => {};
 
-const deleteAnnouncement = async (req, res) => {};
+const deleteAnnouncement = async (req, res) => {
+  const announcementId = req.params.id;
+
+  const announcement = await Announcement.findOneAndRemove({
+    _id: announcementId,
+  });
+
+  if (!announcement)
+    throw new NotFoundError(`No announcement with id ${announcementId}`);
+
+  res.status(StatusCodes.OK).send();
+};
 
 module.exports = {
   getAllAnnouncements,
--- backend/controllers/QuizController.js
+++ backend/controllers/QuizController.js
@@ -2,15 +2,37 @@ const { StatusCodes } = require("http-status-codes");
 const { NotFoundError, BadRequestError } = require("../errors");
 const Quiz = require("../models/Quiz");
 
-const getAllQuizzes = async (req, res) => {};
+const getAllQuizzes = async (req, res) => {
+  const quizzes = await Quiz.find().sort("createdAt");
 
-const getQuiz = async (req, res) => {};
+  res.status(StatusCodes.OK).json({ quizzes, count: quizzes.length });
+};
+
+const getQuiz = async (req, res) => {
+  const quizId = req.params.id;
+
+  const quiz = await Quiz.findOne({ _id: quizId });
+
+  if (!quiz) throw new NotFoundError(`No quiz with id ${quizId}`);
+
+  res.status(StatusCodes.OK).json({ quiz });
+};
 
 const createQuiz = async (req, res) => {};
 
 const updateQuiz = async (req, res) => {};
 
-const deleteQuiz = async (req, res) => {};
+const deleteQuiz = async (req, res) => {
+  const quizId = req.params.id;
+
+  const quiz = await Quiz.findOneAndRemove({
+    _id: quizId,
+  });
+
+  if (!quiz) throw new NotFoundError(`No quiz with id ${quizId}`);
+
+  res.status(StatusCodes.OK).send();
+};
 
 module.exports = {
   getAllQuizzes,
