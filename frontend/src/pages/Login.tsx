c0e0b2e4fc61f3022708514ce26797e30ee10faa
Heisenberg <omaradelawad20013@gmail.com>
2024-02-20 08:23:56 +0200

    added the login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import Card from "../ui/Card";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+import { login } from "../store/slices/authSlice";
+
 const Login = () => {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch(login({ name, password }));
+  };
+
   return (
-    <div>Login</div>
+    <div className="min-h-screen flex flex-row justify-center items-center">
+      <Card className="!py-12">
+        <h2 className="text-2xl text-gray-600 font-medium">Login</h2>
+        <form className="mt-8 flex flex-col gap-6" onSubmit={submitHandler}>
+          <div className="flex flex-col gap-4">
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              value={name}
+              onChange={setName}
+            />
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="*******"
+              value={password}
+              onChange={setPassword}
+            />
+          </div>
+          <Button text="Login" type="submit" />
+        </form>
+      </Card>
+    </div>
   );
 };
 
